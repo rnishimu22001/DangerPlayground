7d20f6c2bcc7a165850ea00356afea459c76d70a
rnishimu22001 <rnishimu22001@yahoo.co.jp>
2020-02-06 00:09:51 +0900

    fix line count

--- code_review.rb
+++ code_review.rb
@@ -26,7 +26,9 @@ module Danger
         def review(file_path, keywords)
             git_info = git.diff_for_file(file_path)
             info = []
+            # git diffのコマンドで表示されるファイルの行情報
             git_start_line = /^@@ .+\+(?<line_number>\d+),/ 
+            # git diffのコマンドで変更された行の先頭に「+」がつく
             # objective-Cのstaticメソッドもひっかかりそう....
             git_modified_line = /^\+/
             line_number = 0
@@ -34,25 +36,24 @@ module Danger
                 start_line_number = 0
                 case line
                 when git_start_line
+                    # 変更開始位置を示す行だった場合は何行目の変更か?を抜き出す
                     start_line_number = Regexp.last_match[:line_number].to_i
+                when git_modified_line
+                    # 変更された行にキーワードが含まれていたら検知した情報を配列に追加
+                    keyword_matched = line.match(Regexp.union(keywords))
+                    if !keyword_matched.nil? then
+                        info << ReviewInfo.new(file_path, line_number, matched.to_a, git_info.patch)
+                    end
                 end
+                
                 if line_number > 0 then
+                    # lineのカウントが始まっていたらカウントするだけ
                     line_number += 1
                 elsif start_line_number > 0 && line_number == 0 then
+                    # lineのカウントが始まっておらず、 カウント開始基準の値が取れていたらline_numberに値を入れてカウントスタート
                     line_number = start_line_number
                 else
                     next
-                end
-                modified_line = line.match(git_modified_line)
-                if modified_line.nil? then
-                    puts "not modified"
-                    next
-                end
-                puts "modified"
-                matched = line.match(Regexp.union(keywords))
-                if !matched.nil? then
-                    puts "detected"
-                    info << ReviewInfo.new(file_path, line_number, matched.to_a, git_info.patch)
                 end
             end
             info
